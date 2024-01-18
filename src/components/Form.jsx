d76d3bf462c54a3074f9226946338e8db45df5e1
Gabriel <gabriel.cafe@ufrpe.br>
2024-01-17 22:07:36 -0300

    Refact.1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,23 +9,25 @@ export default function Form({ addTodo }) {
     const todoObj = { text: text, id: id };
     setId(id + 1);
     addTodo(todoObj);
-    document.getElementById("outlined-basic").value = null;
+    //document.getElementById("outlined-basic").value = null;
   };
 
   return (
     <Paper style={{ padding: "1em" }}>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <TextField
-          id="outlined-basic"
-          label="Tarefa"
-          variant="outlined"
-          onChange={(e) => setText(e.target.value)}
-          fullWidth
-        />
-        <Button variant="text" onClick={() => todoCreate(text)}>
-          Add
-        </Button>
-      </div>
+      <form>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <TextField
+            id="outlined-basic"
+            label="Tarefa"
+            variant="outlined"
+            onChange={(e) => setText(e.target.value)}
+            fullWidth
+          />
+          <Button type="reset" variant="text" onClick={() => todoCreate(text)}>
+            Add
+          </Button>
+        </div>
+      </form>
     </Paper>
   );
 }
